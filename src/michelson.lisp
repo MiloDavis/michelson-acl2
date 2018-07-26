1e5973f6c3e431f44cb3a577d93807b91c79a7aa
Milo Davis <davis.mil@husky.neu.edu>
2018-07-26 11:19:20 -0700

    Fixes ccg import, infrastructure for polymorphic types

--- src/michelson.lisp
+++ src/michelson.lisp
@@ -1,4 +1,6 @@
 (include-book "acl2s/defunc" :dir :system)
+(include-book "acl2s/ccg/ccg" :ttags ((:ccg)) :dir :system :load-compiled-file nil)
+(ld "acl2s/ccg/ccg-settings.lsp" :dir :system)
 
 (in-package "ACL2S")
 
@@ -23,18 +25,14 @@
    s))
 
 
-(defunc make-instr-pred (instr)
-  :input-contract (symbolp instr)
-  :output-contract (symbolp (make-instr-pred instr))
-  (intern-in-package-of-symbol
-   (string-append "INSTR-"
-                  (string-append (string instr)
-                                 "P"))
-   'instr))
-
 (defmacro gen-reified-typep (types type-expr value-expr)
   (if (endp types)
-      nil
+      `(and (mlist-typep ,type-expr)
+            (mlist-datap ,value-expr)
+            (or (endp ,value-expr)
+                (and (reified-typep (mlist-type-type ,type-expr)
+                                    (car ,value-expr))
+                     (reified-typep ,type-expr (cdr ,value-expr)))))
     (let* ((type-info (car types))
            (type (first type-info))
            (pred (third type-info)))
@@ -44,8 +42,16 @@
 
 (defmacro register-mtypes (type-preds)
   `(progn
-     (defdata mtype (oneof ,@(quote* (nth-lol 0 type-preds))))
-     (defdata mdata (oneof ,@(nth-lol 1 type-preds)))
+     (defdata mbase-type (oneof ,@(quote* (nth-lol 0 type-preds))))
+     (defdata (mtype (oneof mbase-type mlist-type))
+       (mlist-type (cons 'LIST mtype)))
+     (defdata mbase-data (oneof ,@(nth-lol 1 type-preds)))
+     (defdata (mdata (oneof mlist-data ,@(nth-lol 1 type-preds)))
+       (mlist-data (list mdata)))
+     (defunc mlist-type-type (lt)
+       :input-contract (mlist-typep lt)
+       :output-contract (mtypep (mlist-type-type lt))
+       (cdr lt))
      (defunc reified-typep (type value)
        :input-contract (and (mtypep type) (mdatap value))
        :output-contract (booleanp (reified-typep type value))
@@ -54,6 +60,7 @@
      (defconst *PRED-LOOKUP*
        (pairlis$ (quote ,(nth-lol 0 type-preds))
                  (quote ,(nth-lol 2 type-preds))))))
+
 (register-mtypes
  ((int integer integerp)
   (string string stringp)
@@ -71,6 +78,15 @@
         ((endp sym) (endp stack))
         (t nil)))
 
+(defunc make-instr-pred (instr)
+  :input-contract (symbolp instr)
+  :output-contract (symbolp (make-instr-pred instr))
+  (intern-in-package-of-symbol
+   (string-append "INSTR-"
+                  (string-append (string instr)
+                                 "P"))
+   'instr))
+
 
 (defmacro declare-primops (primops instrs)
   (if (endp primops)
@@ -85,11 +101,32 @@
 (defdata tc-result (oneof symbolic-stack typerror))
 
 (defdata failgas 'failgas)
-(defdata fail-no-message (cons 'fail string))
+(defdata fail-message (cons 'fail string))
 (defdata fail-raw 'fail)
-(defdata fail (oneof fail-no-message fail-raw))
+(defdata fail (oneof fail-message fail-raw))
 
-(defdata interp-result (oneof stack fail failgas typerror))
+(defdata gas integer)
+
+(defdata gas-cost integer)
+
+(defdata interp-success (cons stack gas))
+
+(defdata interp-result (oneof interp-success fail failgas typerror))
+
+(defunc isuccess (stack gas)
+  :input-contract (and (stackp stack) (gasp gas))
+  :output-contract (interp-successp (isuccess stack gas))
+  (cons stack gas))
+
+(defunc success-stack (success)
+  :input-contract (interp-successp success)
+  :output-contract (stackp (success-stack success))
+  (car success))
+
+(defunc success-gas (success)
+  :input-contract (interp-successp success)
+  :output-contract (gasp (success-gas success))
+  (cdr success))
 
 (defunc transform-sym-stack (checking-stack current-stack output-stack)
   :input-contract (and (symbolic-stackp checking-stack)
@@ -165,22 +202,35 @@
            (,(car accessors) ,stack-expr))
           ,(gen-check-types (cdr types) (cdr accessors) stack-expr))))
 
-(defmacro gen-interpreter-step (primops instr-expr stack)
+(defunc out-of-gasp (gas)
+  :input-contract (gasp gas)
+  :output-contract (booleanp (out-of-gasp gas))
+  (<= gas 0))
+
+(defmacro gen-interpreter-step (primops instr-expr stack gas-expr)
   (if (endp primops)
       (quote 'typerror)
     (let* ((instr-info (car primops))
            (input-stack-types (car (second instr-info)))
            (instr-pred (make-instr-pred (car instr-info)))
            (function (third instr-info))
-           (accessor-ind (- (length input-stack-types) 1)))
+           (gas-func (fourth instr-info))
+           (accessor-ind (- (length input-stack-types) 1))
+           (top-accessors (list-accessors input-stack-types stack)))
       `(if (and (,instr-pred ,instr-expr)
                 (,(nth accessor-ind *STACK-PREDS*) ,stack)
                 ,(gen-check-types input-stack-types *LIST-ACCESSORS* stack))
-           (cons (,function ,@(list-accessors input-stack-types stack))
-                 (,(nth accessor-ind *LIST-REMAINDERS*) ,stack))
+           (let ((remaining-gas (consume-gas ,gas-expr (,gas-func ,@top-accessors))))
+             (if (out-of-gasp remaining-gas)
+                 'failgas
+               (isuccess
+                (cons (,function ,@top-accessors)
+                      (,(nth accessor-ind *LIST-REMAINDERS*) ,stack))
+                remaining-gas)))
          (gen-interpreter-step ,(cdr primops)
                                ,instr-expr
-                               ,stack)))))
+                               ,stack
+                               ,gas-expr)))))
 
 (defmacro primops (ops)
   `(progn
@@ -189,10 +239,12 @@
        :input-contract (and (primopp instr) (symbolic-stackp stack))
        :output-contract (tc-resultp (typechecker-step instr stack))
        (gen-typechecker-step ,ops instr stack))
-     (defunc interpreter-step (instr stack)
-       :input-contract (and (primopp instr) (stackp stack))
-       :output-contract (interp-resultp (interpreter-step instr stack))
-       (gen-interpreter-step ,ops instr stack))))
+     (defunc interpreter-step (instr stack gas)
+       :input-contract (and (primopp instr) (stackp stack) (gasp gas))
+       :output-contract (interp-resultp (interpreter-step instr stack gas))
+       (if (out-of-gasp gas)
+           'failgas
+           (gen-interpreter-step ,ops instr stack gas)))))
 
 (defunc cmp-int (n1 n2)
   :input-contract (and (integerp n1) (integerp n2))
@@ -201,28 +253,61 @@
         ((= n1 n2) 0)
         (t -1)))
 
+
 (defunc cmpres-eq (c)
   :input-contract (integerp c)
   :output-contract (booleanp (cmpres-eq c))
   (= c 0))
 
+(defunc consume-gas (gas cost)
+    :input-contract (and (gas-costp cost) (gasp gas))
+    :output-contract (gasp (consume-gas gas cost))
+    (- gas cost))
+
+;; TODO: find better solution to unused parameters
+(progn
+  (set-ignore-ok t)
+  (set-irrelevant-formals-ok t)
+
+  (defunc preliminary-gas-unop (x)
+    :input-contract t
+    :output-contract (gas-costp (preliminary-gas-unop x))
+    1)
+
+  (defunc preliminary-gas-binop (x1 x2)
+    :input-contract t
+    :output-contract (gas-costp (preliminary-gas-binop x1 x2))
+    1)
+
+  (set-ignore-ok nil)
+  (set-irrelevant-formals-ok nil))
+
 (primops
- ((add ((int int) . (int)) +)
-  (sub ((int int) . (int)) -)
-  (mul ((int int) . (int)) *)
+ ((add ((int int) . (int)) + preliminary-gas-binop)
+  (sub ((int int) . (int)) - preliminary-gas-binop)
+  (mul ((int int) . (int)) * preliminary-gas-binop)
 
-  (and ((bool bool) . (bool)) and)
-  (or ((bool bool) . (bool)) or)
+  (and ((bool bool) . (bool)) and preliminary-gas-binop)
+  (or ((bool bool) . (bool)) or preliminary-gas-binop)
 
-  (cmp ((int int) . (int)) cmp-int)
-  (eq  ((int) . (bool)) cmpres-eq)))
+  (cmp ((int int) . (int)) cmp-int preliminary-gas-binop)
+  (eq  ((int) . (bool)) cmpres-eq preliminary-gas-unop)))
 
 (defthm typechecker-step-progress
   (implies (and (symbolic-stackp sym-stack)
                 (stackp stack)
                 (primopp op)
                 (reified-stackp sym-stack stack)
+                (gasp gas)
                 (not (typerrorp (typechecker-step op sym-stack))))
-           (and (reified-stackp (typechecker-step op sym-stack)
-                                (interpreter-step op stack))
-                (not (typerrorp (interpreter-step op stack))))))
+           (let ((stepped (interpreter-step op stack gas)))
+             (or (failgasp stepped)
+                 (and (not (typerrorp stepped))
+                      (reified-stackp (typechecker-step op sym-stack)
+                                      (success-stack stepped)))))))
+
+(defdata
+  (instr (oneof primop instr-dip instr-if))
+  (instr-dip (cons 'DIP instr-seq))
+  (instr-if (list* 'IF instr-seq instr-seq))
+  (instr-seq (listof instr)))
